376d42350a5bb6a43b72695228f42b2ca39a7059
Aaron Woo <awoo@chapman.edu>
2022-04-23 10:37:12 -0700

    Successfully retrieved weather forecast data for 5 days.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,47 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
-  const [data, setWeatherData] = useState('none');
-  let response = fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=7f4ddd7fea09e7bc32942448d698c69e')
-    .then(response => response.json())
-    .then(data => console.log(data));
 
-  // function setWeather(data) {
-  //   setWeatherData(data);
-  // }
+  useEffect(() => {
+    async function fetchWeather() {
+      let forecastResponse = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Orange&appid=7f4ddd7fea09e7bc32942448d698c69e&units=imperial');
+      let forecastData = await forecastResponse.json();
+      console.log('forecast data:', forecastData)
 
-  // useEffect(() => {
-  //   setWeatherData(response);
-  // })
+      let todaysWeather = {};
+      todaysWeather.max = forecastData.list[0].main.temp_max;
+      todaysWeather.min = forecastData.list[0].main.temp_min;
+      
+      console.log('todaysWeather obj', todaysWeather);
+
+      
+    }
+
+    fetchWeather();
+
+  }, []);
 
 
   return (
     <div>
       <p>
-        Weather data:
+        Weather data: ?
       </p>
     </div>
   )
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+
 }
 
 export default App;
