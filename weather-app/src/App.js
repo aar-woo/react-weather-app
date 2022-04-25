047f3c94e8491c9642d43f534394dbea49b4fa78
Aaron Woo <awoo@chapman.edu>
2022-04-24 18:35:07 -0700

    Changed api call to call for seven day forecast because 4 day forecast data returned variable amounts of data for each day.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,40 +5,28 @@ function App() {
   const [forecastData, setForecastData] = useState([{}]);
 
   async function fetchWeather() {
-    let forecastResponse = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Orange&appid=7f4ddd7fea09e7bc32942448d698c69e&units=imperial');
+    let forecastResponse = await fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,alerts&appid=7f4ddd7fea09e7bc32942448d698c69e&units=imperial')
     let forecastData = await forecastResponse.json();
-    let todaysWeather = {};
-    const options = {weekday: 'long'}
-
-    todaysWeather.date = forecastData.list[0].dt_txt;
-    todaysWeather.maxTemp = forecastData.list[0].main.temp_max;
-    todaysWeather.minTemp = forecastData.list[0].main.temp_min;
-    todaysWeather.weatherIcon = forecastData.list[0].weather[0].description;
-    const todaysDate = new Date(todaysWeather.date);
-    // todaysWeather.weekday = todaysDate.getDay();
-    // todaysWeather.weekday = new Int1.DateTimeFormat('en-US', options).format(todaysDate);
-
     const parsedForecastData = [];
-    parsedForecastData.push(todaysWeather);
-    for (let i = 1; i < 26; i += 8) {
+    // const options = {weekday: 'long'}
+
+    console.log('forecast', forecastData);
+
+    for (let i = 0; i < 5; i++) {
       const day = {};
-      const currDayForecast = forecastData.list[i];
+      const currDayForecast = forecastData.daily[i];
      
-      day.date = currDayForecast.dt_txt;
-      day.maxTemp = currDayForecast.main.temp_max;
-      day.minTemp = currDayForecast.main.temp_min;
+      day.unixDate = currDayForecast.dt;
+      day.maxTemp = currDayForecast.temp.max;
+      day.minTemp = currDayForecast.temp.min;
       day.weatherIcon = currDayForecast.weather[0].description;
 
-      const daysDate = new Date(day.date);
+      const daysDate = new Date(day.unixDate);
       day.weekday = daysDate.getDay();
       parsedForecastData.push(day);
     }
-    const today = new Date(parsedForecastData[0].date);
-    const weekday = today.getDay();
-    console.log('weekday num', weekday)
-    console.log('parseed', parsedForecastData)
 
-    setForecastData(parsedForecastData);
+    console.log("parsed", parsedForecastData)
   }
 
   useEffect(() => {
