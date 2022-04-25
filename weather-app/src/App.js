a0e50d7d0a92dfaf25215ce8c2eb97f68ebaa3f0
Aaron Woo <awoo@chapman.edu>
2022-04-24 18:10:00 -0700

    Changed initial forecast data to be an array of an empty obj to prevent initial render from erroring.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,36 +2,46 @@ import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
-  const [forecastData, setForecastData] = useState([]);
+  const [forecastData, setForecastData] = useState([{}]);
+
+  async function fetchWeather() {
+    let forecastResponse = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Orange&appid=7f4ddd7fea09e7bc32942448d698c69e&units=imperial');
+    let forecastData = await forecastResponse.json();
+    let todaysWeather = {};
+    const options = {weekday: 'long'}
+
+    todaysWeather.date = forecastData.list[0].dt_txt;
+    todaysWeather.maxTemp = forecastData.list[0].main.temp_max;
+    todaysWeather.minTemp = forecastData.list[0].main.temp_min;
+    todaysWeather.weatherIcon = forecastData.list[0].weather[0].description;
+    const todaysDate = new Date(todaysWeather.date);
+    // todaysWeather.weekday = todaysDate.getDay();
+    // todaysWeather.weekday = new Int1.DateTimeFormat('en-US', options).format(todaysDate);
+
+    const parsedForecastData = [];
+    parsedForecastData.push(todaysWeather);
+    for (let i = 1; i < 26; i += 8) {
+      const day = {};
+      const currDayForecast = forecastData.list[i];
+     
+      day.date = currDayForecast.dt_txt;
+      day.maxTemp = currDayForecast.main.temp_max;
+      day.minTemp = currDayForecast.main.temp_min;
+      day.weatherIcon = currDayForecast.weather[0].description;
+
+      const daysDate = new Date(day.date);
+      day.weekday = daysDate.getDay();
+      parsedForecastData.push(day);
+    }
+    const today = new Date(parsedForecastData[0].date);
+    const weekday = today.getDay();
+    console.log('weekday num', weekday)
+    console.log('parseed', parsedForecastData)
+
+    setForecastData(parsedForecastData);
+  }
 
   useEffect(() => {
-    async function fetchWeather() {
-      let forecastResponse = await fetch('https://api.openweathermap.org/data/2.5/forecast?q=Orange&appid=7f4ddd7fea09e7bc32942448d698c69e&units=imperial');
-      let forecastData = await forecastResponse.json();
-      let todaysWeather = {};
-
-      todaysWeather.date = forecastData.list[0].dt_txt;
-      todaysWeather.maxTemp = forecastData.list[0].main.temp_max;
-      todaysWeather.minTemp = forecastData.list[0].main.temp_min;
-      todaysWeather.weatherIcon = forecastData.list[0].weather[0].description;
-
-      const parsedForecastData = [];
-      parsedForecastData.push(todaysWeather);
-      for (let i = 1; i < 26; i += 8) {
-        const day = {};
-        const currDayForecast = forecastData.list[i];
-        day.date = currDayForecast.dt_txt;
-        day.maxTemp = currDayForecast.main.temp_max;
-        day.minTemp = currDayForecast.main.temp_min;
-        day.weatherIcon = currDayForecast.weather[0].description;
-        parsedForecastData.push(day);
-      }
-
-      setForecastData(parsedForecastData);
-      
-      console.log('parsed', parsedForecastData)
-    }
-
     fetchWeather();
 
   }, []);
@@ -46,6 +56,7 @@ function App() {
         <div className='min-max-div justify-evenly flex'>
           <span>{Math.round(forecastData[0].minTemp)}°</span>
           <span className='grey'>{Math.round(forecastData[0].maxTemp)}</span>
+
         </div>
       </div>
 
